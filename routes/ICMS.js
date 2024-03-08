d1b991d0dc8d880eca7bbd21dfb6dece821b1a17
yehan2002 <yehanjaya2002@gmail.com>
2024-03-08 20:52:27 +0530

    ICMS/ward: Move request code to system directory.

--- routes/ICMS.js
+++ routes/ICMS.js
@@ -3,33 +3,9 @@ const router = express.Router();
 
 const wardService = require("../systems/ICMS/wards");
 
-router.post("/ward", async (req, res) => {
-    let name = req.query.name;
-    // TODO: validate name
-
-    await wardService.create(name);
-    res.sendStatus(200);
-});
-
-router.get("/ward", async (req, res) => {
-    res.status(200).json(await wardService.get());
-});
-
-router.patch("/ward/:id", async (req, res) => {
-    let id = req.params.id;
-    let name = req.body.name;
-    // TODO: validate
-
-    await wardService.renameWard(id, name);
-    res.sendStatus(200);
-});
-
-router.delete("/ward/:id", async (req, res) => {
-    let id = req.params.id;
-    // TODO: validate
-
-    await wardService.delete(id);
-    res.sendStatus(200);
-});
+router.post("/ward", wardService.create);
+router.get("/ward", wardService.get);
+router.patch("/ward/:id", wardService.renameWard);
+router.delete("/ward/:id", wardService.delete);
 
 module.exports = router;
--- systems/ICMS/wards.js
+++ systems/ICMS/wards.js
@@ -1,32 +1,68 @@
 const Ward = require("../../model/ward");
+const express = require("express");
 
-async function getWards() {
-    return await Ward.findAll();
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function getWards(req, res) {
+    res.status(200).json(await Ward.findAll());
 }
 
-async function createWard(name) {
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function createWard(req, res) {
+    let name = req.query.name;
+    // TODO: validate name
+
+    await wardService.create(name);
     await Ward.create({ name: name });
+    res.status(200).json({ ok: true, msg: "Ward created successfully" });
 }
 
-async function renameWard(id, newName) {
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function renameWard(req, res) {
+    let id = req.params.id;
+    let name = req.body.name;
+    // TODO: validate
+
+
     let ward = await Ward.findByPk(id);
     if (ward === null) {
-        return false;
+        res.status(404).json({ ok: false, msg: "The ward does not exist" })
+        return;
     }
 
     ward.name = newName;
     await ward.save();
+    res.status(200).json({ ok: true, msg: "Ward renamed successfully" });
 }
 
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function deleteWard(req, res) {
+    let id = req.params.id;
+    // TODO: validate
 
-
-async function deleteWard(id) {
     let ward = await Ward.findByPk(id);
     if (ward === null) {
-        return false;
+        res.status(404).json({ ok: false, msg: "The ward does not exist" })
+        return;
     }
 
     await ward.destroy();
+    res.status(200).json({ ok: true, msg: "Ward deleted successfully" });
 }
 
 module.exports = { create: createWard, get: getWards, renameWard: renameWard, delete: deleteWard }
