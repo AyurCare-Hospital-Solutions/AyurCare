e812e51e71af1ae0ac46094803364cadc9060860
yehan2002 <yehanjaya2002@gmail.com>
2024-03-08 23:07:32 +0530

    Update tests.

--- test/routes/ICMS.test.js
+++ test/routes/ICMS.test.js
@@ -13,12 +13,36 @@ describe('ICMS Endpoints', () => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toHaveProperty('ok', true);
         expect(res.body).toHaveProperty('data.name', "test");
-    })
+    });
 
     it("GET /icms/ward returns a list of wards", async () => {
         const res = await request.get("/icms/ward");
         expect(res.statusCode).toBe(200);
         expect(res.body).toHaveProperty('ok', true);
         expect(res.body).toHaveProperty('data');
-    })
+    });
+
+    it("PATCH /icms/ward/:id renames ward", async () => {
+        let res = await request.post('/icms/ward').send({ name: "test 1234" });
+        expect(res.body).toHaveProperty('data.id');
+        expect(res.body).toHaveProperty('data.name', "test 1234");
+
+        let id = res.body.data.id;
+        res = await request.patch(`/icms/ward/${id}`).send({ name: "new name" });
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty("data.name", "new name");
+    });
+
+    it("DELETE /icms/ward/:id deletes the ward", async () => {
+        let res = await request.post('/icms/ward').send({ name: "delete test" });
+        expect(res.body).toHaveProperty('data.id');
+        expect(res.body).toHaveProperty('data.name', "delete test");
+
+        let id = res.body.data.id;
+        res = await request.delete(`/icms/ward/${id}`);
+
+        expect(res.statusCode).toBe(200);
+
+    });
 })
