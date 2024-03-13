171f17e734a1054e40627d2aa2687da4fd36e4ea
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-13 14:59:50 +0530

    Tender request model created

---
+++ backend/model/TenderRequest.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+const Item = require("./Item");
+
+const TenderRequest = sequelize.define("Tender_Requests",
+    {
+        reqID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+        },
+        status: {
+            type: DataTypes.STRING,
+            validate: {
+                isIn: [["Succcess", "Pending", "Reject"]]
+            }
+        }
+    }
+);
+
+Item.hasMany(TenderRequest, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    foreignKey: { allowNull: false }
+});
+TenderRequest.belongsTo(Item);
+
+module.exports = TenderRequest;
