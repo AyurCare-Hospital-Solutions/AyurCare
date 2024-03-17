e453b5997d1652db81aab539e7f231ce30ba59a6
lahirurupa <lahirurupasinghe@gmail.com>
2024-03-17 15:50:30 +0530

    partial request crud.

--- backend/routes/DMS.js
+++ backend/routes/DMS.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const medi = require("../systems/DMMS/med");
+
+router.get("/request",medi.getRequests);
+router.post("/request",medi.createRequest);
+router.delete("/request/:id",medi.deleteRequest);
 
 // Register routes here
 
---
+++ backend/systems/DMMS/med.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const yup = require("yup");
+const ManufactureRequest = require("../../model/ManufactureRequest");
+const Medicine = require("../../model/Medicine");
+
+
+const requestValidator = yup.object({
+    amount : yup.number().integer().min(1).max(500).required(),
+    isPriority : yup.boolean().required(),
+    itemId : yup.number().integer().required(),
+
+}).strict().noUnknown();
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function getRequests(req, res) {
+    res.status(200).json(await ManufactureRequest.findAll());
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function createRequest(req, res) {
+    try {
+        var data = await requestValidator.validate(req.body);
+    } catch (validationError) {
+        res.status(400).send({ msg: validationError.errors[0] });
+        return;
+    }
+
+    let medicine = await Medicine.findByPk(data.itemId);
+    if(medicine === null){
+        res.status(400).json({ msg: "Invalid Medicine Id" });
+        return;
+    }
+
+    let requ = await ManufactureRequest.create({
+        amount:data.amount,
+        isPriority:data.isPriority,
+        itemId:data.itemId
+    })
+
+    res.status(200).json({data:requ.toJSON()});
+}
+
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+async function deleteRequest(req, res) {
+    let reqId = Number.parseInt(req.params.id);
+
+    // check if Request number is valid
+    if (!Number.isInteger(reqId)) {
+        res.status(400).json({ msg: "Invalid Request number" });
+        return;
+    }
+
+    // get the Request with the given id
+    let request = await ManufactureRequest.findByPk(id);
+    if (request === null) {
+        res.status(404).json({ msg: "The Request does not exist" })
+        return;
+    }
+
+    await request.destroy();
+    res.sendStatus(204);
+}
+
+module.exports = { getRequests, createRequest, deleteRequest}
