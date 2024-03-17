bda7d64fe4468455f419a1328fd9eab256ad2f8a
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-17 14:35:04 +0530

    medicine CRUD

--- backend/routes/IMS.js
+++ backend/routes/IMS.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const medicineService = require('../systems/IMS/medicine')
 
 // Register routes here
+
+// Medicine 
+router.get('/medicine',medicineService.getMedicine);
+router.post('/medicine/addMedicine', medicineService.addMedicine);
+router.put('/medicine/updateMedicine/:id', medicineService.updateMedicine);
+router.post('/medicine/deleteMedicine', medicineService.deleteMedicine);
+
+// Material
+
+// Accessories
+
+// Request
 
 module.exports = router;
