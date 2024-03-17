3c13d3798a6415ba1e8e3e06679a759fdcfcbb3e
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-17 16:58:34 +0530

    material crud

--- backend/routes/IMS.js
+++ backend/routes/IMS.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const materialService = require('../systems/IMS/material');
 
 // Register routes here
+
+router.get('/material', materialService.getMaterial);
+router.post('/material/addMaterial', materialService.addMaterial);
+router.put('/material/updateMaterial/:id', materialService.updateMaterial);
+router.post('/material/deleteMaterial', materialService.deleteMaterial);
 
 module.exports = router;
