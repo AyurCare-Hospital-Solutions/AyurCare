920be6ff3b96854e42e4e2b6bb01c29104e88388
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-18 12:47:30 +0530

    accessory crud

--- backend/routes/IMS.js
+++ backend/routes/IMS.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const accessoryService = require('../systems/IMS/accessory')
 
 // Register routes here
 
+router.get('/accessory',accessoryService.getAccessory);
+router.post('/accessory/addAccessory',accessoryService.addAccessory);
+router.put('/accessory/updateAccessory/:id',accessoryService.updateAccessory);
+router.post('/accessory/deleteAccessory',accessoryService.deleteAccessory);
 
 module.exports = router;
