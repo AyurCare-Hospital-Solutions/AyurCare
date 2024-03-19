03c31625ebfd55ac02d1a44b803096ae75c05b56
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-19 13:55:00 +0530

    medicine table added

---
+++ frontend/src/systems/IMS/Medicine.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//@ts-ignore
+import MedicineTable from "./component/MedicineTable"
+
+function Medicine() {
+  return (
+    <div>
+      <MedicineTable/>
+    </div>
+  )
+}
+
+export default Medicine
--- frontend/src/systems/IMS/index.tsx
+++ frontend/src/systems/IMS/index.tsx
@@ -1,15 +1,15 @@
 import { RouteObject } from "react-router-dom";
-// @ts-ignore
-import MedicineTable from "./component/MedicineTable"
+import ImsMain from "./ImsMain"
+import Medicine from "./Medicine";
 
 const routes: { name: String, root: React.ReactNode, routes: RouteObject[] } = {
     name: "IMS",
-    root: <p>IMS</p>, // replace with main element
+    root: <ImsMain />, // replace with main element
     routes: [
         // Register routes here
         {
-            path : "medicine",
-            element: <MedicineTable/>
+            path: "medicine",
+            element: <Medicine />
         }
     ],
 }
