73e8b397bdd169a8c2d8c003bdc66a6791fdf00f
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-19 12:00:21 +0530

    prs frontend root file restructured

--- frontend/src/systems/PRS/index.tsx
+++ frontend/src/systems/PRS/index.tsx
@@ -1,15 +1,13 @@
 import { RouteObject } from "react-router-dom";
 
 
+const routes: { name: String, root: React.ReactNode, routes: RouteObject[] } = {
+    name: "PRS",
+    root: <p>PRS</p>, // replace with main element
+    routes: [
+        // Register routes here
 
-export default function (): { name: String, root: React.ReactNode, routes: RouteObject[] } {
-    return {
-        name: "PRS",
-        root: <p>PRS</p>, // replace with main element
-        routes: [
-            // Register routes here
-
-        ],
-    }
-
+    ],
 }
+
+export default routes;
