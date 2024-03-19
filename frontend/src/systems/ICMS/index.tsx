9337ca0ede00d51e2f72450694d963de919027b3
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-19 11:57:53 +0530

    icms frontend root file restructured

--- frontend/src/systems/ICMS/index.tsx
+++ frontend/src/systems/ICMS/index.tsx
@@ -1,15 +1,12 @@
 import { RouteObject } from "react-router-dom";
 
+const routes: { name: String, root: React.ReactNode, routes: RouteObject[] } = {
+    name: "ICMS",
+    root: <p>ICMS</p>, // replace with main element
+    routes: [
+        // Register routes here
 
-
-export default function (): { name: String, root: React.ReactNode, routes: RouteObject[] } {
-    return {
-        name: "ICMS",
-        root: <p>ICMS</p>, // replace with main element
-        routes: [
-            // Register routes here
-
-        ],
-    }
-
+    ],
 }
+
+export default routes;
