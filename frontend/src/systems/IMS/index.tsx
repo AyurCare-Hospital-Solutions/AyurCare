94ca29a688e888626dccc2c77f1fd3ff55755e08
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-19 11:58:04 +0530

    ims frontend root file restructured

--- frontend/src/systems/IMS/index.tsx
+++ frontend/src/systems/IMS/index.tsx
@@ -1,15 +1,12 @@
 import { RouteObject } from "react-router-dom";
 
+const routes: { name: String, root: React.ReactNode, routes: RouteObject[] } = {
+    name: "IMS",
+    root: <p>IMS</p>, // replace with main element
+    routes: [
+        // Register routes here
 
-
-export default function (): { name: String, root: React.ReactNode, routes: RouteObject[] } {
-    return {
-        name: "IMS",
-        root: <p>IMS</p>, // replace with main element
-        routes: [
-            // Register routes here
-
-        ],
-    }
-
+    ],
 }
+
+export default routes;
