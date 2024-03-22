0da183935c30c281cb0c68908be3c172be3e0b85
yehan2002 <yehanjaya2002@gmail.com>
2024-03-22 18:48:22 +0530

    Install notistack.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import Nav from "./components/Nav.tsx";
 import "./index.css";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { SnackbarProvider } from 'notistack';
 
 const theme = createTheme({
   palette: {
@@ -20,8 +21,10 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Nav></Nav>
-      <App></App>
+      <SnackbarProvider autoHideDuration={3000} >
+        <Nav></Nav>
+        <App></App>
+      </SnackbarProvider>
       test
     </ThemeProvider>
   </React.StrictMode>,
