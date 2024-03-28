e3eb2811482feb080a402ad4660bcf291d4b2803
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-28 19:47:50 +0530

    conflicts

---
+++ frontend/src/systems/IMS/ImsMain.tsx
@@ -0,0 +1,42 @@
+
+import { Dashboard, Inbox, Mail } from '@mui/icons-material';
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+
+import { Link, Outlet } from 'react-router-dom'
+import DashboardPage from '../../components/DashboardPage';
+
+
+function ImsMain() {
+
+    return (
+<<<<<<< HEAD
+        <div>
+            <DashboardPage>
+                <Link to="/ims/dashboard">
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Dashboard />
+                            </ListItemIcon>
+                            <ListItemText primary="Dashboard" />
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+
+                <Link to="/ims/medicine">
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <VaccinesIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Medicine" />
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+            </DashboardPage>
+        </div >
+    )
+}
+
+export default ImsMain
