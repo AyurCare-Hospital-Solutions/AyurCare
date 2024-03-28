3a6397bacc07c27f57446a068ac4ced1718176bf
Navindu Nimsara <navidunimsara@gmail.com>
2024-03-28 19:48:01 +0530

    conflicts

--- frontend/src/systems/IMS/ImsMain.tsx
+++ frontend/src/systems/IMS/ImsMain.tsx
@@ -2,7 +2,6 @@
 import { Dashboard, Inbox, Mail } from '@mui/icons-material';
 import VaccinesIcon from '@mui/icons-material/Vaccines';
 import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-
 import { Link, Outlet } from 'react-router-dom'
 import DashboardPage from '../../components/DashboardPage';
 
