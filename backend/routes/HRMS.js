8b324d1b6a3f23f4300b98a8b060e3c4904e98c9
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-03-30 13:52:54 +0530

    Leave Request Routes

--- backend/routes/HRMS.js
+++ backend/routes/HRMS.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// Register routes here
+const leaveService = require("../systems/HRMS/leaves");
+
+router.get("/leave", leaveService.getAllLeaveRequests);
+router.get("/leave/:id", leaveService.getLeaveRequestById);
+router.post("/leave", leaveService.createLeaveRequest);
+router.put("/leave/:id", leaveService.updateLeaveRequest);
+router.delete("/leave/:id", leaveService.deleteLeaveRequest);
 
 module.exports = router;
