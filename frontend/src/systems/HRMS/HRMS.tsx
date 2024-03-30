091c9e9e8c53cdccd7c33493a42679f6bd4c0c08
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-03-30 15:52:14 +0530

    Leave Request Sidebar

---
+++ frontend/src/systems/HRMS/HRMS.tsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+import DashboardPage from "../../components/DashboardPage";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { Assessment, Dashboard, Event, Groups } from "@mui/icons-material";
+
+const HRMS = () => {
+  return (
+    <DashboardPage>
+      <Link to="/HRMS/Dashboard">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Dashboard />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItemButton>
+        </ListItem>
+      </Link>
+
+      <Link to="/HRMS/Dashboard">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Event />
+            </ListItemIcon>
+            <ListItemText primary="Leave Management" />
+          </ListItemButton>
+        </ListItem>
+      </Link>
+
+      <Link to="/HRMS/Dashboard">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Groups />
+            </ListItemIcon>
+            <ListItemText primary="Roster Management" />
+          </ListItemButton>
+        </ListItem>
+      </Link>
+      
+      <Link to="/HRMS/Dashboard">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Assessment />
+            </ListItemIcon>
+            <ListItemText primary="Reports" />
+          </ListItemButton>
+        </ListItem>
+      </Link>
+    </DashboardPage>
+  );
+};
+
+export default HRMS
