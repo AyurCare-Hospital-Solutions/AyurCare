89676e3139b995b4f630ae1f57e085e6d01a90f9
Navindu Nimsara <navidunimsara@gmail.com>
2024-04-02 12:52:06 +0530

    accessories title added

--- frontend/src/systems/IMS/Accessories.tsx
+++ frontend/src/systems/IMS/Accessories.tsx
@@ -1,11 +1,16 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 function Accessories() {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+    return (
+        <div>
+            <Box>
+                <Typography variant='h3' color='primary' align="center">
+                    Medicine
+                </Typography>
+            </Box>
+        </div>
+    )
 }
 
 export default Accessories
