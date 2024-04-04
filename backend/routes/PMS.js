ca36e64182e670cfe8f567aa3772b4bd1528bff4
Vinushann <vinushan.vimalraj@gmail.com>
2024-04-04 12:02:07 +0530

    Final touch

--- backend/routes/PMS.js
+++ backend/routes/PMS.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const medicine = require("../systems/PMS/medicine")
-const prescription = require("../systems/PMS/prescription")
+const medicine = require("../systems/PMS/medicine");
+const prescription = require("../systems/PMS/prescription");
 
-router.get("/medicine", medicine.getMedicines)
-router.get("/prescriptionDetails", prescription.getPrescriptions);
+// here we have mention if the url (first parameter) call this function that's it.
+// those methods are inside the **** systems/pms ****
 
 router.get("/medicineall", medicine.getPharmacyMedicines);
 router.get("/specificmedicine/:id", medicine.getSpecificMedicine);
@@ -13,10 +13,7 @@ router.post("/updatemedicinestock/:id", medicine.changeStockLevel);
 router.get("/getInventoryMedicine", medicine.getInventoryMedicine);
 router.post("/setPharmacyMadicine", medicine.setPharmacyMadicine);
 
-
-
 router.get("/getAllPrescription", prescription.getPrescriptions);
 router.get("/updatePrescription/:id", prescription.updatePrescription);
-
 
 module.exports = router;
