a394f80eb9d28d91783cd69cc1a28866429b2e3a
yehan2002 <yehanjaya2002@gmail.com>
2024-04-04 14:04:28 +0530

    Use card components.

--- frontend/src/systems/ICMS/Dashboard.tsx
+++ frontend/src/systems/ICMS/Dashboard.tsx
@@ -1,50 +1,13 @@
-import { ArrowCircleRightOutlined } from "@mui/icons-material";
 import { Box, Grid, Paper, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import AdvancedFormat from "dayjs/plugin/advancedFormat";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { DateCard, LinkCard, WelcomeCard } from "../../components/DashboardCards";
 
 dayjs.extend(AdvancedFormat)
 
 
 const cardStyle = { display: "flex", px: "16px", py: "12px" }
 
-const LinkCard = ({ name, desc, link }: { name: string, desc: string, link: string }) => {
-    return <Link to={link} style={{ textDecoration: "none" }}>
-        <Paper elevation={3} sx={{ ...cardStyle, mt: 2, height: "94px", mr: "auto", minWidth: "80%" }}>
-            <Box sx={{ my: "auto" }}>
-                <Typography sx={{ fontSize: "1.75rem", fontWeight: 500 }}>{name}</Typography>
-                <Typography sx={{ fontWeight: 500, }}>{desc}</Typography>
-            </Box>
-            <Box sx={{ ml: "auto", my: "auto" }}>
-                <ArrowCircleRightOutlined fontSize="large" />
-            </Box>
-        </Paper>
-    </Link>
-}
-
-const DateCard = () => {
-    const [time, setTime] = useState(new Date());
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(new Date())
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-
-    return <Paper elevation={3} sx={{ ...cardStyle, minHeight: "140px", mb: 4, backgroundColor: "#d9d9d9" }}>
-        <Box sx={{ my: "auto" }}>
-            <Typography sx={{ fontWeight: 400 }}>Today is</Typography>
-            <Typography sx={{ fontSize: "1.75rem", fontWeight: 500 }}>{dayjs(time).format("Do MMMM YYYY")}</Typography>
-        </Box>
-    </Paper>
-
-
-}
 
 const Dashboard = () => {
     return <>
@@ -56,14 +19,7 @@ const Dashboard = () => {
                 </Box>
             </Grid>
             <Grid item xs={5}>
-                <Paper elevation={3} sx={{ ...cardStyle, backgroundColor: "#cde1b4", mb: 4, minHeight: "140px" }}>
-                    <Box sx={{ my: "auto" }}>
-                        <Typography sx={{ color: "#003a2b", fontWeight: 600, fontSize: "1.8rem" }} >Welcome Back!</Typography>
-                        <Typography sx={{ color: "#003a2b", fontSize: "1.1rem" }} >Have a Nice Day!</Typography>
-                    </Box>
-                    <Box flexGrow="1"></Box>
-                    <img style={{ alignSelf: "end" }} src="/assets/ICMS-dash-icon.svg"></img>
-                </Paper >
+                <WelcomeCard img="/assets/ICMS-dash-icon.svg" />
             </Grid>
             <Grid item xs={5}>
                 <DateCard />
