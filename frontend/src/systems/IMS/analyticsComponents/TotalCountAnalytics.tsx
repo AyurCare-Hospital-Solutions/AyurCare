5ed7c77649e9661a963b0af9a4541fac18cc3052
Navindu Nimsara <navidunimsara@gmail.com>
2024-04-14 00:49:06 +0530

    total counts pie chart and bar chart complete

--- frontend/src/systems/IMS/analyticsComponents/TotalCountAnalytics.tsx
+++ frontend/src/systems/IMS/analyticsComponents/TotalCountAnalytics.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import { BarChart } from '@mui/x-charts';
 import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -14,7 +15,7 @@ const getTotalCount = async () => {
 
 export const TotalCountPieChart = () => {
 
-    const [totalCountData, setTotalCountData] = useState<any>({});
+    const [totalCountData, setTotalCountData] = useState<any>();
 
     useEffect(() => {
         getTotalCount()
@@ -28,9 +29,9 @@ export const TotalCountPieChart = () => {
     }, []);
 
     const data = [
-        { id: 0, value: totalCountData.medicineCount, label: 'Medicine' },
-        { id: 1, value: totalCountData.materialCount, label: 'Material' },
-        { id: 2, value: totalCountData.accessoryCount, label: 'Accessory' },
+        { id: 0, value: totalCountData ? totalCountData.medicineCount : 0, label: 'Medicine' },
+        { id: 1, value: totalCountData ? totalCountData.materialCount : 0, label: 'Material' },
+        { id: 2, value: totalCountData ? totalCountData.accessoryCount : 0, label: 'Accessory' },
     ];
 
     return (
@@ -39,7 +40,7 @@ export const TotalCountPieChart = () => {
                 series={[
                     {
                         outerRadius: 80,
-                        arcLabel: (item) => `${(item.value * 100 / totalCountData.itemCount).toFixed(2)}%`,
+                        arcLabel: (item) => `${(item.value * 100 / (totalCountData ? totalCountData.itemCount : 0)).toFixed(2)}%`,
                         data,
                     },
                 ]}
@@ -51,10 +52,49 @@ export const TotalCountPieChart = () => {
                 }}
                 width={400}
                 height={200}
-                loadingAnimationDuration={10000} 
+                loadingAnimationDuration={10000}
             />
             <Typography align="center">
                 pie chart: Inventory Item Category Distribution
+            </Typography>
+        </Box>
+    );
+}
+
+export const TotalCountBarChart = () => {
+    const [totalCountData, setTotalCountData] = useState<any>();
+
+    useEffect(() => {
+        getTotalCount()
+            .then((data) => {
+                setTotalCountData(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }, []);
+
+    const data = [
+        { id: 0, value: totalCountData ? totalCountData.medicineCount : 0, label: 'Medicine' },
+        { id: 1, value: totalCountData ? totalCountData.materialCount : 0, label: 'Material' },
+        { id: 2, value: totalCountData ? totalCountData.accessoryCount : 0, label: 'Accessory' },
+    ];
+
+    return (
+        <Box width={500} height={300}>
+            <BarChart
+                colors={['#ab4896']}
+                xAxis={[{ scaleType: 'band', dataKey: 'label', label: "Category" }]}
+                yAxis={[{ label: "Count" }]}
+                dataset={data}
+                series={[{ dataKey: 'value', label: 'Category distribution' }]}
+                width={500}
+                height={300}
+                sx={{ marginTop: '2rem' }}
+            />
+            <Typography align="center">
+                bar chart: Inventory Item Category Distribution
             </Typography>
         </Box>
     );
