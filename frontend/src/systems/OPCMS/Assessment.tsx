97ed9605d7d7373c3f05fe1e8138a08bb7aafd18
ayowee <pasinduayomal2001@gmail.com>
2024-04-20 12:07:05 +0530

    Merge branch 'main' of https://github.com/AyurCare-Hospital-Solutions/AyurCare into Ayomal_Patient_assesment

---
+++ frontend/src/systems/OPCMS/Assessment.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Assessment = () => {
+  return (
+    <div>A</div>
+  )
+}
+
+export default Assessment;
--- frontend/src/systems/OPCMS/index.tsx
+++ frontend/src/systems/OPCMS/index.tsx
@@ -1,11 +1,14 @@
 import { RouteObject } from "react-router-dom";
+import Assessment from "./Assessment";
 
 const routes: { name: String, root: React.ReactNode, routes: RouteObject[] } = {
     name: "OCMS",
     root: <p>OCMS</p>, // replace with main element
     routes: [
-        // Register routes here
-
+        {
+            path:"/assesment",
+            element:<Assessment />
+        }
     ],
 }
 
