1e5b961c71966e2b6aa8562f5ff6d8449370e1c0
yehan2002 <yehanjaya2002@gmail.com>
2024-04-21 13:34:47 +0530

    Update MedicineMaterial model.

--- backend/model/MedicineMaterial.js
+++ backend/model/MedicineMaterial.js
@@ -3,17 +3,23 @@ const { sequelize } = require(".");
 const Medicine = require("./Medicine");
 const Material = require("./Material");
 
-const MedicineMaterial = sequelize.define("MedicineMaterial", {}, { timestamps: false });
+const MedicineMaterial = sequelize.define("MedicineMaterial", {
+    amount: DataTypes.NUMBER
+}, { timestamps: false });
 
-MedicineMaterial.belongsTo(Medicine,
-    {
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
-        foreignKey: { allowNull: false, }
-    },
-);
+MedicineMaterial.belongsTo(Medicine, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+    foreignKey: { allowNull: false, }
+});
 
-MedicineMaterial.belongsToMany(Material, { through: "MedicineMaterialMaterials", timestamps: false });
+MedicineMaterial.belongsTo(Material, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+    foreignKey: { allowNull: false, }
+})
 
+
+MedicineMaterial.sync({ force: true })
 
 module.exports = MedicineMaterial;
