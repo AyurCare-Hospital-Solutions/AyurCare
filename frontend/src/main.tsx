96e70464a2dac40121028bfe369acf91d8ae57fd
yehan2002 <yehanjaya2002@gmail.com>
2024-04-23 22:37:46 +0530

    Remove express and nodemon from frontend.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -37,5 +37,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </SnackbarProvider>
       </LocalizationProvider>
     </ThemeProvider>
-  </React.StrictMode >,
+  </React.StrictMode >
 )
