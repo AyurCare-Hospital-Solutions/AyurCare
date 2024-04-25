33976b12f9221e17350d13fbfe9eb0295d4850a8
ayowee <pasinduayomal2001@gmail.com>
2024-04-25 22:25:31 +0530

    sample get method

--- frontend/src/systems/OPCMS/Assessment.tsx
+++ frontend/src/systems/OPCMS/Assessment.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 
 const Assessment = () => {
-  return (
-    <div>Fgrvgerags</div>
-  )
-}
+  const[assesmentData,setAssesmentData] = useState();
+  
+  // get assesment details
+const getAssessmentDetails = () => {
+  axios
+    .get("api/opcms/patientAssessment")
+    .then((res) => {
+      console.log(res.data);
+      setAssesmentData(res.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+useEffect(() => {
+  getAssessmentDetails();
+},[]);
+
+  return <div>Fgrvgerags</div>;
+};
 
 export default Assessment;
