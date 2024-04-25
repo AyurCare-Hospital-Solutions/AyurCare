eea20459a0ab47f81c379a97291de82fe2da72b0
ayowee <pasinduayomal2001@gmail.com>
2024-04-25 21:09:13 +0530

    Refactor DashboardCards component in frontend/src/components/DashboardCards.tsx

--- frontend/src/components/DashboardCards.tsx
+++ frontend/src/components/DashboardCards.tsx
@@ -12,7 +12,21 @@ const cardStyle = { display: "flex", px: "16px", py: "12px" }
 
 export const LinkCard = ({ name, desc, link }: { name: string, desc: string, link: string }) => {
     return <Link to={link} style={{ textDecoration: "none", overflow: "hidden" }}>
-        <Paper elevation={3} sx={{ ...cardStyle, mt: 2, height: "94px", mr: "auto", minWidth: "80%" }}>
+        <Paper elevation={1} sx={{ ...cardStyle, mt: 2, height: "max-content", mr: "auto", minWidth: "80%", borderRadius: "15px" }}>
+            <Box sx={{ my: "auto" }}>
+                <Typography sx={{ fontSize: "1.75rem", fontWeight: 500 }}>{name}</Typography>
+                <Typography sx={{ fontWeight: 500, }}>{desc}</Typography>
+            </Box>
+            <Box sx={{ ml: "auto", my: "auto" }}>
+                <ArrowCircleRightOutlined fontSize="large" />
+            </Box>
+        </Paper>
+    </Link>
+}
+
+export const LinkCard2 = ({ name, desc, link }: { name: string, desc: string, link: string }) => {
+    return <Link to={link} style={{ textDecoration: "none", overflow: "hidden" }}>
+        <Paper elevation={1} sx={{ ...cardStyle, mt: 2, height: "330px", mr: "auto", minWidth: "80%", borderRadius: "15px" }}>
             <Box sx={{ my: "auto" }}>
                 <Typography sx={{ fontSize: "1.75rem", fontWeight: 500 }}>{name}</Typography>
                 <Typography sx={{ fontWeight: 500, }}>{desc}</Typography>
@@ -36,7 +50,7 @@ export const DateCard = () => {
     }, []);
 
 
-    return <Paper elevation={3} sx={{ ...cardStyle, minHeight: "140px", mb: 4, backgroundColor: "#d9d9d9" }}>
+    return <Paper elevation={1} sx={{ ...cardStyle, height: "150px", mb: "1px", backgroundColor: "#d9d9d9", borderRadius: "15px", mt: "10px"}}>
         <Box sx={{ my: "auto" }}>
             <Typography sx={{ fontWeight: 400 }}>Today is</Typography>
             <Typography sx={{ fontSize: "1.75rem", fontWeight: 500 }}>{dayjs(time).format("Do MMMM YYYY HH:mm:ss")}</Typography>
@@ -45,12 +59,14 @@ export const DateCard = () => {
 }
 
 export const WelcomeCard = ({ img }: { img: string }) => {
-    return <Paper elevation={3} sx={{ ...cardStyle, backgroundColor: "#cde1b4", mb: 4, minHeight: "140px", overflowX: "hidden" }}>
-        <Box sx={{ my: "auto" }}>
-            <Typography sx={{ color: "#003a2b", fontWeight: 600, fontSize: "1.8rem" }} >Welcome Back!</Typography>
-            <Typography sx={{ color: "#003a2b", fontSize: "1.1rem" }} >Have a Nice Day!</Typography>
-        </Box>
-        <Box flexGrow="1"></Box>
-        <img style={{ alignSelf: "end" }} src={img}></img>
-    </Paper >
+    return (
+        <Paper elevation={1} sx={{ ...cardStyle, backgroundColor: "#cde1b4", mb: "1px", height: "150px", overflowX: "hidden", borderRadius: "15px", mt: "10px" }}>
+            <Box sx={{ my: "auto" }}>
+                <Typography sx={{ color: "#003a2b", fontWeight: 600, fontSize: "1.8rem" }} >Welcome Back!</Typography>
+                <Typography sx={{ color: "#003a2b", fontSize: "1.1rem",  }} >Have a Nice Day!</Typography>
+            </Box>
+            <Box flexGrow="1"></Box>
+            <img style={{ alignSelf: "end", width: "auto", height: "150px" }} src={img} alt="Welcome Image" />
+        </Paper>
+    );
 }
--- frontend/src/systems/OPCMS/Dashboard.tsx
+++ frontend/src/systems/OPCMS/Dashboard.tsx
@@ -1,44 +1,29 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import dayjs from "dayjs";
 import AdvancedFormat from "dayjs/plugin/advancedFormat";
-import { DateCard, LinkCard, WelcomeCard } from "../../components/DashboardCards";
+import { DateCard, LinkCard, LinkCard2, WelcomeCard } from "../../components/DashboardCards";
 
 dayjs.extend(AdvancedFormat)
-
-
-const cardStyle = { display: "flex", px: "16px", py: "12px" }
-
 
 const Dashboard = () => {
     return <>
         <Grid container spacing={2} justifyContent="space-evenly">
-            <Grid item xs={12}>
-                <Box sx={{ my: 2.5, mx: 2, color: "#003a2b" }} >
-                    <Typography variant="h4" >Welcome to</Typography>
-                    <Typography variant="h5" sx={{ pl: 4 }}>Inpatient Management Dashboard</Typography>
-                </Box>
-            </Grid>
-            <Grid item xs={5}>
-                <WelcomeCard img="/assets/ICMS-dash-icon.svg" />
-            </Grid>
-            <Grid item xs={5}>
-                <DateCard />
-            </Grid>
             <Grid item xs={6}>
-                <Paper sx={{ ...cardStyle }}>
-                    Waiting list
-                </Paper>
+                <WelcomeCard img="/assets/Doctor-pana.svg" />
             </Grid>
             <Grid item xs={4}>
-
+                <DateCard />
+            </Grid>
+            <Grid item xs={4}>
+                <LinkCard2 name="Appointments" desc="Patient Analysis and Medical States Analysis" link="/opcms/appointments"></LinkCard2>
+            </Grid>
+            <Grid item xs={6}>
                 <Box flexGrow="3" display="flex" flexDirection="column" >
-                    <LinkCard name="Care Plans" desc="View Patient Care Plans" link="/icms/care_plan"></LinkCard>
-                    <LinkCard name="Nursing Logs" desc="View Nursing Logs" link="/icms/nursing_log"></LinkCard>
-                    <LinkCard name="Reports" desc="Generate Reports" link="/icms/reports"></LinkCard>
-
+                    <LinkCard name="Medical Records" desc="Find Medical Record details" link="/icms/care_plan"></LinkCard>
+                    <LinkCard name="Reports" desc="Generate reports" link="/icms/nursing_log"></LinkCard>
+                    <LinkCard name="Analytics" desc="Patient Analysis and Medical States Analysis" link="/opcms/analytics"></LinkCard>
                 </Box>
             </Grid>
-
         </Grid >
     </>
 };
