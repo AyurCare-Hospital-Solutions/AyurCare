17ae205e3d39db26b13a512425397d2d11af3ffe
ayowee <pasinduayomal2001@gmail.com>
2024-04-25 13:34:42 +0530

    Refactor OPCMS Dashboard component in frontend/src/systems/OPCMS/Dashboard.tsx

--- frontend/src/systems/OPCMS/Dashboard.tsx
+++ frontend/src/systems/OPCMS/Dashboard.tsx
@@ -1,36 +1,46 @@
-import { Box, Stack } from '@mui/material'
-import { DateCard, LinkCard, WelcomeCard } from '../../components/DashboardCards'
+import { Box, Grid, Paper, Typography } from "@mui/material";
+import dayjs from "dayjs";
+import AdvancedFormat from "dayjs/plugin/advancedFormat";
+import { DateCard, LinkCard, WelcomeCard } from "../../components/DashboardCards";
 
-function Dashboard() {
-    return (
-        <div>
-        <Box
-            m={5}
-            sx={{ 
-                display: 'flex',
-                borderRadius: 15,
-            }}
-        >
-            <Box mr={4} sx={{ flexGrow: 1 }}><WelcomeCard img="/assets/" /></Box>
-            <Box>
-            <DateCard />
-            </Box>
-        </Box>
-        <Box width="100%">
-            <Stack direction="row" spacing={6} alignItems="center" justifyContent="center" >
-            <Box>
-                <LinkCard name={"Medical Records"} desc={"Find Medical Record details"} link={"/opcms/records"} />
-            </Box>
-            <Box>
-                <LinkCard name={"Reports"} desc={"Generate reports"} link={"opcms/reports"} />
-            </Box>
-            <Box>
-                <LinkCard name={"Analysis"} desc={""} link={"opcms/analysis"} />
-            </Box>
-            </Stack>
-        </Box>
-        </div>
-    )
-}
+dayjs.extend(AdvancedFormat)
+
+
+const cardStyle = { display: "flex", px: "16px", py: "12px" }
+
+
+const Dashboard = () => {
+    return <>
+        <Grid container spacing={2} justifyContent="space-evenly">
+            <Grid item xs={12}>
+                <Box sx={{ my: 2.5, mx: 2, color: "#003a2b" }} >
+                    <Typography variant="h4" >Welcome to</Typography>
+                    <Typography variant="h5" sx={{ pl: 4 }}>Inpatient Management Dashboard</Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={5}>
+                <WelcomeCard img="/assets/ICMS-dash-icon.svg" />
+            </Grid>
+            <Grid item xs={5}>
+                <DateCard />
+            </Grid>
+            <Grid item xs={6}>
+                <Paper sx={{ ...cardStyle }}>
+                    Waiting list
+                </Paper>
+            </Grid>
+            <Grid item xs={4}>
+
+                <Box flexGrow="3" display="flex" flexDirection="column" >
+                    <LinkCard name="Care Plans" desc="View Patient Care Plans" link="/icms/care_plan"></LinkCard>
+                    <LinkCard name="Nursing Logs" desc="View Nursing Logs" link="/icms/nursing_log"></LinkCard>
+                    <LinkCard name="Reports" desc="Generate Reports" link="/icms/reports"></LinkCard>
+
+                </Box>
+            </Grid>
+
+        </Grid >
+    </>
+};
 
 export default Dashboard;
