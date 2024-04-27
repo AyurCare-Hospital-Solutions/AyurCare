d6b4988b04713504e9bb16bc894e71fbfcd0771e
yehan2002 <yehanjaya2002@gmail.com>
2024-04-27 18:38:08 +0530

    Install react-to-pdf.

--- frontend/src/components/ReportPage.tsx
+++ frontend/src/components/ReportPage.tsx
@@ -3,7 +3,7 @@ import { Button, Typography } from "@mui/material";
 import { ReactElement, useEffect, useRef, useState } from "react";
 import generatePDF, { Margin } from "react-to-pdf";
 
-const ReportPage = ({ children, filename, title }: { children: ReactElement | ReactElement[], filename: string, title: string }) => {
+const ReportPage = ({ children, filename, title, visible }: { children: ReactElement | ReactElement[], visible?: boolean, filename: string, title: string }) => {
     const [isPrint, setIsPrinting] = useState(false);
 
     useEffect(() => {
@@ -16,7 +16,7 @@ const ReportPage = ({ children, filename, title }: { children: ReactElement | Re
     const ref = useRef<any>();
     return <>
         <Button onClick={() => setIsPrinting(true)} startIcon={<Print />}>Download</Button>
-        {isPrint ? <div ref={ref} style={{ padding: "4em 2em" }}>
+        {isPrint || visible ? <div ref={ref} style={{ padding: "4em 2em" }}>
             <Typography textAlign="center" variant="h6">{title}</Typography>
             {children}
         </div> : null}
