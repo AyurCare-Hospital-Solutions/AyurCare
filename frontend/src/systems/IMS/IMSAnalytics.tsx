869b0bf3315cbe26dea5cda344ec665e32cb007d
Navindu Nimsara <navidunimsara@gmail.com>
2024-04-28 12:45:58 +0530

    analytics updated ---> report download

--- frontend/src/systems/IMS/IMSAnalytics.tsx
+++ frontend/src/systems/IMS/IMSAnalytics.tsx
@@ -9,100 +9,119 @@ import { AccessoryGroupBarChart, AccessoryGroupPieChart } from './analyticsCompo
 import { AccessoryStockLevelBarChart, AccessoryStockLevelPieChart } from './analyticsComponents/AccessoryStockLevelAnalytics';
 import { MedicineRequestStatusBarChart, MedicineRequestStatusPieChart } from './analyticsComponents/MedicineRequestAnalyrtics';
 import { MaterialRequestStatusBarChart, MaterialRequestStatusPieChart } from './analyticsComponents/MaterialRequestAnalytics';
+import ReportGenerator from '../../components/ReportGenerator';
+import dayjs from 'dayjs';
 
 function IMSAnalytics() {
+  const getDate = () => {
+    const date = dayjs().format('YYYY-MM-DD');
+    const time = dayjs().format('HH:MM');
+    return { date, time };
+  }
   return (
     <div>
-      <Box>
-        <Typography variant='h3' color='primary' align="center">
-          Inventory Analytics
-        </Typography>
-      </Box>
+      <ReportGenerator filename='Inventory Analytics' title='' visible titleHidden>
+        <Box>
+          <Typography variant='h3' color='primary' align="center">
+            Inventory Analytics
+          </Typography>
+        </Box>
 
-      <Box mt={4}>
-        <Typography variant='h5' color='primary' >
-          Total stocks in each item category
-        </Typography>
-        <Stack direction="row" >
-          <TotalCountPieChart />
-          <TotalCountBarChart />
-        </Stack>
-      </Box>
+        <Box mt={10}>
+          <Typography color='secondary' align="right">
+            Generated Date : {getDate().date}
+          </Typography>
+          <Typography color='secondary' align="right">
+            Generated Time : {getDate().time}
+          </Typography>
+        </Box>
 
-      <Box mt={10}>
-        <Typography variant='h5' color='primary'>
-          Medicine lot count for each medicine
-        </Typography>
-        <MedicineLotBarChart />
-      </Box>
 
-      <Box mt={10} >
-        <Typography variant='h5' color='primary'>
-          Medicine lot status analytics
-        </Typography>
-        <Stack direction="row" >
-          <MedicineStockLevelPieChart />
-          <MedicineStockLevelBarChart />
-        </Stack>
-      </Box>
-      <Box mt={10}>
-        <Typography variant='h5' color='primary'>
-          Material stock analytics (based on measurement unit)
-        </Typography>
-        <Stack direction='row' >
-          <MaterialGroupsPieChart />
-          <MaterialGroupBarChart />
-        </Stack>
-      </Box>
-      <Box mt={10}>
-        <Typography variant='h5' color='primary'>
-          Material stock status analytics
-        </Typography>
-        <Stack direction='row' >
-          <MaterialStockLevelPieChart />
-          <MaterialStockLevelBarChart />
-        </Stack>
-      </Box>
+        <Box mt={4}>
+          <Typography variant='h5' color='primary' >
+            Total stocks in each item category
+          </Typography>
+          <Stack direction="row" >
+            <TotalCountPieChart />
+            <TotalCountBarChart />
+          </Stack>
+        </Box>
 
-      <Box mt={10} >
-        <Typography variant='h5' color='primary'>
-          Accessory stock analytics (based on measurement unit)
-        </Typography>'
-        <Stack direction='row' >
-          <AccessoryGroupPieChart />
-          <AccessoryGroupBarChart />
-        </Stack>
-      </Box>
+        <Box mt={10}>
+          <Typography variant='h5' color='primary'>
+            Medicine lot count for each medicine
+          </Typography>
+          <MedicineLotBarChart />
+        </Box>
 
-      <Box mt={10}>
-        <Typography variant='h5' color='primary'>
-          Accessory stock status analytics
-        </Typography>
-        <Stack direction='row' >
-          <AccessoryStockLevelPieChart />
-          <AccessoryStockLevelBarChart />
-        </Stack>
-      </Box>
+        <Box mt={10} >
+          <Typography variant='h5' color='primary'>
+            Medicine lot status analytics
+          </Typography>
+          <Stack direction="row" >
+            <MedicineStockLevelPieChart />
+            <MedicineStockLevelBarChart />
+          </Stack>
+        </Box>
+        <Box mt={10}>
+          <Typography variant='h5' color='primary'>
+            Material stock analytics (based on measurement unit)
+          </Typography>
+          <Stack direction='row' >
+            <MaterialGroupsPieChart />
+            <MaterialGroupBarChart />
+          </Stack>
+        </Box>
+        <Box mt={10}>
+          <Typography variant='h5' color='primary'>
+            Material stock status analytics
+          </Typography>
+          <Stack direction='row' >
+            <MaterialStockLevelPieChart />
+            <MaterialStockLevelBarChart />
+          </Stack>
+        </Box>
 
-      <Box mt={10}>
-        <Typography variant='h5' color='primary'>
-          Medicine request status analytics
-        </Typography>
-        <Stack direction='row' >
-          <MedicineRequestStatusPieChart />
-          <MedicineRequestStatusBarChart />
-        </Stack>
-      </Box>
+        <Box mt={10} >
+          <Typography variant='h5' color='primary'>
+            Accessory stock analytics (based on measurement unit)
+          </Typography>'
+          <Stack direction='row' >
+            <AccessoryGroupPieChart />
+            <AccessoryGroupBarChart />
+          </Stack>
+        </Box>
 
-      <Box mt={10}>
-        <Typography variant='h5' color='primary'>
-          Material request status analytics
-        </Typography>
-        <Stack direction='row'>
-          <MaterialRequestStatusPieChart />
-          <MaterialRequestStatusBarChart />
-        </Stack>
-      </Box>
+        <Box mt={10}>
+          <Typography variant='h5' color='primary'>
+            Accessory stock status analytics
+          </Typography>
+          <Stack direction='row' >
+            <AccessoryStockLevelPieChart />
+            <AccessoryStockLevelBarChart />
+          </Stack>
+        </Box>
+
+        <Box mt={10}>
+          <Typography variant='h5' color='primary'>
+            Medicine request status analytics
+          </Typography>
+          <Stack direction='row' >
+            <MedicineRequestStatusPieChart />
+            <MedicineRequestStatusBarChart />
+          </Stack>
+        </Box>
+
+        <Box mt={10}>
+          <Typography variant='h5' color='primary'>
+            Material request status analytics
+          </Typography>
+          <Stack direction='row'>
+            <MaterialRequestStatusPieChart />
+            <MaterialRequestStatusBarChart />
+          </Stack>
+        </Box>
+      </ReportGenerator>
     </div>
   )
 }
