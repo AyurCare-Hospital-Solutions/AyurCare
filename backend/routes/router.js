5816dd145fe9d8f665e7a986f71024d24edd7824
yehan2002 <yehanjaya2002@gmail.com>
2024-05-12 12:27:07 +0530

    Rename PRS path.

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -29,6 +29,6 @@ register("/icms", "./ICMS");
 register("/ims", "./IMS");
 register("/ocms", "./OCMS");
 register("/pms", "./PMS");
-register("/prs", "./PRS");
+register("/prs", "./PRSS");
 
 module.exports = router;
